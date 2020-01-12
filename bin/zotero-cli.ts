60af052aa5093dc6cf0151e3480ee095ff9c3507
Bjoern <bjohas@users.noreply.github.com>
2020-01-12 17:20:23 +0000

    Added: collection --items
    Added: collection --items to show items in collection.

--- bin/zotero-cli.ts
+++ bin/zotero-cli.ts
@@ -260,7 +260,8 @@ class Zotero {
 
     if (argparser) {
       argparser.addArgument('--key', { required: true,  help: 'The key of the item.' })
-      argparser.addArgument('--tags', { action: 'storeTrue', help: 'Display present in the collection.' })
+      argparser.addArgument('--tags', { action: 'storeTrue', help: 'Display tags present in the collection.' })
+      argparser.addArgument('--items', { action: 'storeTrue', help: 'Display items present in the collection.' })
       argparser.addArgument('--add', { action: 'storeTrue', help: 'Add items to this collection.' })
       argparser.addArgument('items', { nargs: '*'})
       return
@@ -268,6 +269,14 @@ class Zotero {
 
     if (this.args.tags && this.args.add) {
       this.parser.error('--tags cannot be combined with --add')
+      return
+    }
+    if (this.args.items && this.args.add) {
+      this.parser.error('--items cannot be combined with --add')
+      return
+    }
+    if (this.args.items && this.args.tags) {
+      this.parser.error('--items and --tags cannot be combined.')
       return
     }
     if (this.args.add && !this.args.items.length) {
@@ -287,8 +296,11 @@ class Zotero {
       }
       return
     }
-
-    this.show(await this.get(`/collections/${this.args.key}${this.args.tags ? '/tags' : ''}`))
+    if (this.args.items) {
+        this.show(await this.get(`/collections/${this.args.key}/items`))
+    } else {
+        this.show(await this.get(`/collections/${this.args.key}${this.args.tags ? '/tags' : ''}`))
+    };
   }
 
   async $collections(argparser = null) {
